07f43da58a381a5cdaa3782eb72c67f4bec41159
wangjz <wangjinzhou@btte.net>
2019-03-08 22:30:36 +0800

    unit test code update

--- libconfig_test.go
+++ libconfig_test.go
@@ -1,8 +1,20 @@
 package libconfig
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
 
 func TestParseConfigFile(t *testing.T) {
-	ParseConfigFile("/Users/wangjz/Desktop/work/project/opensource/janus-gateway/conf/janus.jcfg.sample.in")
+	jsonStr,err := ParseConfigFile("/Users/wangjz/Desktop/work/project/opensource/janus-gateway/conf/janus.jcfg.sample.in")
+	if err!=nil{
+		fmt.Errorf(err.Error())
+	}
+	fmt.Println(jsonStr)
+	fmt.Println("=========================")
+	var jsonMap map[string]interface{}
+	json.Unmarshal([]byte(jsonStr),&jsonMap)
+	fmt.Println(jsonMap)
 	//ParseJsonConfigFile("/Users/wangjz/Desktop/janus.jcfg.sample.in")
 }
